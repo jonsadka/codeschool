27407fabb701452ab9381f23b8ff0a3cf103dfd0
Jon Sadka <jonsadka@gmail.com>
2014-09-19 10:53:01 -0700

    Update practice problems from eloquent javascript ch 4

--- javascript path/EloquentJS_ch4.js
+++ javascript path/EloquentJS_ch4.js
@@ -55,19 +55,14 @@ var countBs = function(string){
 // Write a range function that takes two arguments, start and end, and returns an array containing all the numbers from start up to (and including) end.
 // Next, write a sum function that takes an array of numbers and returns the sum of these numbers. Run the previous program and see whether it does indeed return 55.
 // As a bonus assignment, modify your range function to take an optional third argument that indicates the “step” value used to build up the array. If no step is given, the array elements go up by increments of one, corresponding to the old behavior. The function call range(1, 10, 2) should return [1, 3, 5, 7, 9]. Make sure it also works with negative step values so that range(5, 2, -1) produces [5, 4, 3, 2].
-var range = function(start, end, step){
-  var result = [];
-  var count = 0;
+var createWithReduce = function(start, stop, step){
+  var iterationCount = Array.apply(null, new Array((stop-start + 1)/step) );
   step = step || 1;
-  if ( end <= start ) return "End larger than or equal to start";
-  while (count < end-start ){
-    result.push(null);
-    count += step;
-  }
-  return result.reduce(function(pv,cv,i){
-    pv.push( start + i*step + 1 );
-    return pv;
-  }, [start]);
+
+  return iterationCount.reduce(function(previousValue, currentValue, index){
+    previousValue.push( start + index*step );
+    return previousValue;
+  },[] );
 };
 var sum = function(arr){
   startValue = 0;
